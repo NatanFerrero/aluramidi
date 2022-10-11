a77cd0c333abb2514ba707f5054c986478e98902
Nata Correia Santos <nata.correia@totvs.com.br>
2022-10-10 22:54:32 -0300

    final step

--- main.js
+++ main.js
@@ -4,22 +4,25 @@ function tocaSom (idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 //enquanto
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`;
-    console.log(idAudio);
-
+    const idAudio = `#som_${instrumento}`; //template string
+    
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1; 
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
 
-    console.log(contador);
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
 }  
+
+
